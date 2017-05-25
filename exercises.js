92e816fd47781a5f4eef85321f67bf7f2ceb7b50
Ryan Colvin <ryancolvin@Ryans-Mac-Pro.local>
2017-05-25 14:48:20 -0700

    update

--- exercises.js
+++ exercises.js
@@ -11,8 +11,8 @@ function multiplyByTen(num) {
 function subtractFive(num) {
   //return num after subtracting five
   //code here
-    var division = num - 5;
-    return division;
+    var difference = num - 5;
+    return difference;
     
 }
 
@@ -20,50 +20,81 @@ function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
   //code here
+    if (str1.length === str2.length) {
+        return true;
+    }
+    return false;
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
   //code here
-    //HI
+    
+    
+  if (x === y) {
+      return true;
+  }
+    return false;
 }
+
+
+
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
   //code here
+    if (num < 90) {
+        return true;
+    }
+    return false;
+        
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
   //code here
+    if (num > 50) {
+        return true;
+    }
+    return false;
 }
 
 function add(x, y) {
   //add x and y together and return the value
   //code here
+    var sum = x + y;
+    return sum;
 }
 
 function subtract(x, y) {
   //subtract y from x and return the value
   //code here
+    var difference = x - y;
+    return difference;
 }
 
 function divide(x, y) {
   //divide x by y and return the value
   //code here
+    var division = x / y;
+    return division;
 }
 
 function multiply(x, y) {
   //multiply x by y and return the value
   //code here
+    var multiplication = x * y;
+    return multiplication;
 }
 
 function getRemainder(x, y) {
   //return the remainder from dividing x by y
   //code here
+    var remainder = x % y;
+    return remainder;
 }
 
 function isEven(num) {
@@ -81,44 +112,60 @@ function isOdd(num) {
 function square(num) {
   //square num and return the new value
   //code here
+    var squareNum = Math.pow(num, 2);
+    return squareNum;
 }
 
 function cube(num) {
   //cube num and return the new value
   //code here
+    var cubeNum = Math.pow(num, 3);
+    return cubeNum;
 }
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
   //code here
+    var raisePower = Math.pow(num, 5);
+    return raisePower;
 }
 
 function roundNumber(num) {
   //round num and return it
   //code here
+    var roundNum = Math.round(num);
+    return roundNum;
 }
 
 function roundUp(num) {
   //round num up and return it
   //code here
+    var roundUpNum = Math.ceil(num);
+    return roundUpNum;
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
   //code here
+    var excl = str + '!';
+    return excl;
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
   //code here
+    var fullName = firstName + ' ' + lastName;
+    return fullName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
   //code here
+    var greeting = 'Hello ' + name + '!';
+    return greeting;
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
